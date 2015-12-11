20d114826825d48ad3955d8cdccfccb0b9704b96
Paul Biester <p.biester@isonet.fr>
2015-12-11 22:08:49 +0100

    init

---
+++ app.js
@@ -0,0 +1,37 @@
+http = require('http');
+fs = require('fs');
+
+
+
+port = 3000;
+host = '0.0.0.0';
+
+server = http.createServer(function(req, res) {
+
+    if (req.method == 'POST') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+
+        var body = '';
+        req.on('data', function(data) {
+            body += data;
+        });
+        req.on('end', function() {
+            console.log(body);
+            res.end('');
+        });
+
+    } else {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        var html = 'yes';
+        res.end(html);
+    }
+
+});
+
+
+server.listen(port);
+console.log('Listening at ttp://' + host + ':' + port);
